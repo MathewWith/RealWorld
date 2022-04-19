cbef06906eb386912b582a7cd313194d0fa380da
Matthew Ovsyannikov <v.pg.98@mail.ru>
2022-04-19 12:32:25 +0300

    Fixed mistakes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,31 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <div className="header">
-            
-            <div className='header__nav'>
-                <Link className='header__nav--logo' to={'/'}>
-                    conduit
-                </Link>
-                <ul className='header__nav-item'>
-                    <li>
-                        <Link className='link' to={'/'}>
-                            Home
-                        </Link>
-                    </li>
-                
-                    <li>
-                        <Link className='link' to={'/sign-in'}>
-                            Sign in
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className='link' to={'/sign-up'}>
-                            Sing up
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header__nav">
+        <Link className="header__nav--logo" to={"/"}>
+          conduit
+        </Link>
+        <ul className="header__nav--items">
+          <li>
+            <Link className="nav-item" to={"/"}>
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link className="nav-item" to={"/sign-in"}>
+              Sign in
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-item" to={"/sign-up"}>
+              Sing up
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};

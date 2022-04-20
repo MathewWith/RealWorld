a4c00c0579783c6525d47090921e14f08a8860f2
Matthew Ovsyannikov <v.pg.98@mail.ru>
2022-04-20 13:25:39 +0300

    Fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,29 +3,27 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <div className="header">
-      <div className="header__nav">
-        <Link className="header__nav--logo" to={"/"}>
+        <Link className="header__logo" to={"/"}>
           conduit
         </Link>
-        <ul className="header__nav--items">
+        <ul className="header__nav">
           <li>
-            <Link className="nav-item" to={"/"}>
+            <Link className="header__nav--item" to={"/"}>
               Home
             </Link>
           </li>
 
           <li>
-            <Link className="nav-item" to={"/sign-in"}>
+            <Link className="header__nav--item" to={"/sign-in"}>
               Sign in
             </Link>
           </li>
           <li>
-            <Link className="nav-item" to={"/sign-up"}>
+            <Link className="header__nav--item" to={"/sign-up"}>
               Sing up
             </Link>
           </li>
         </ul>
-      </div>
     </div>
   );
 };
--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,9 +1,9 @@
 
 export const Home = () => {
     return (
-        <div className="home--page">
-            <div className="home--page__banner">
-                <div className="home--page__banner--content">
+        <div className="main--page">
+            <div className="main--page__banner">
+                <div className="main--page__banner--content">
                     <h1 className="banner__content--label">conduit</h1>
                     <p className="banner__content--description">A place to share your knowledge</p>
                 </div>

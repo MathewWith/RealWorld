67adb1e12cdf15dc3a92ab1f624438f0142f7d64
Matthew Ovsyannikov <v.pg.98@mail.ru>
2022-04-20 15:23:16 +0300

    Fixed

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -5,8 +5,8 @@ export const Home = () => {
         <div className="main-page">
             <div className="main-page__banner">
                 <div className="main-page__banner-content">
-                    <h1 className="banner__content-label">conduit</h1>
-                    <p className="banner__content-description">A place to share your knowledge</p>
+                    <h1 className="banner-content__label">conduit</h1>
+                    <p className="banner-content__description">A place to share your knowledge</p>
                 </div>
             </div>
             <Articles />
